a3f4cbe7098fa7e1de08a92d94da37a2b7d55390
tsqware <tsqware@gmail.com>
2020-08-10 19:58:17 -0400

    Hid navbar toggle, removed sql file

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -15,9 +15,11 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="/">tsqware INTERACTIVE</a>
+            <!--
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            -->
         </nav>
         
             @yield('main')
